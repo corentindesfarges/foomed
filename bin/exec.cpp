92476cfeea4a04ae581f3eb0478601fcf77f45c0
Corentin Desfarges <corentin.desfarges.dev@gmail.com>
2015-02-19 11:16:22 +0100

    initial commit

---
+++ bin/exec.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+#include "lib.hpp"
+
+int main(){
+	std::cout << somme(10,2) << std::endl;
+	return 0;
+}
---
+++ lib/lib.cpp
@@ -0,0 +1,5 @@
+#include "lib.hpp"
+
+int somme(int a, int b){
+	return a+b;
+}
